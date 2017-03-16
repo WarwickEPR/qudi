de921c8cce055c6ccbc5fac762c8cc84e644d3f2
DESKTOP-4FUS4CC\Confocal <b.green@warwick.ac.uk>
2017-03-16 16:07:29 +0000

    Fix issue with spinboxes

--- qtwidgets/pyqtgraphmod/SpinBox.py
+++ qtwidgets/pyqtgraphmod/SpinBox.py
@@ -6,6 +6,7 @@ from pyqtgraph.SignalProxy import SignalProxy
 from pyqtgraph import functions as fn
 from math import log
 from decimal import Decimal as D  ## Use decimal to avoid accumulating floating-point errors
+from decimal import ROUND_FLOOR
 #from decimal import *
 import weakref
 
