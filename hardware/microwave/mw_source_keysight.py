585259a87cb6db3ab68306a4166ae09530fb24be
Ben Green (Confocal) <b.green@warwick.ac.uk>
2017-05-09 11:16:14 +0100

    Don't pass the Fysom state object on activation inline with the latest changes upstream e.g. in ni_card.py

--- hardware/microwave/mw_source_keysight.py
+++ hardware/microwave/mw_source_keysight.py
@@ -41,17 +41,8 @@ class MicrowaveKeysight(Base, MicrowaveInterface):
     _modclass = 'MicrowaveKeysight'
     _modtype = 'hardware'
 
-    def on_activate(self,e):
-        """ Initialisation performed during activation of the module.
-
-        @param object e: Event class object from Fysom.
-                         An object created by the state machine module Fysom,
-                         which is connected to a specific event (have a look in
-                         the Base Class). This object contains the passed event,
-                         the state before the event happened and the destination
-                         of the state which should be reached after the event
-                         had happened.
-        """
+    def on_activate(self):
+        """ Initialisation performed during activation of the module."""
         # checking for the right configuration
         config = self.getConfiguration()
         # agilent mw source has USB connection, therefore configuration checks for USB address
