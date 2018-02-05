c0d6e97172e629fbd3a3ecafd6d105e89d9fbc5d
Ben Green (Confocal) <b.green@warwick.ac.uk>
2018-02-05 15:39:22 +0000

    A script to set up triggers to pause pulsed measurements when refocusin. Could be rolled in to startup

---
+++ set_up_pulsed_bright_refocus.py
@@ -0,0 +1,37 @@
+
+try:
+    pulsedmasterlogic
+except:
+    print("Pulsed modules not loaded")
+
+try:
+    optimizerlogic
+except:
+    print("Optimizerlogic not loaded")
+
+# Create handlers for refocusing that pause a running pulsed measurement
+
+def pulsed_running():
+    try:
+        return pulsedmasterlogic.status_dict['measurement_running']
+    except:
+        return False
+
+def pulsed_paused():
+    try:
+        return pulsed_running() and not pulsedmasterlogic.status_dict['pulser_running']
+    except:
+        return False
+
+def on_refocusing(tag):
+    if pulsed_running() and not pulsed_paused():
+        pulsedmasterlogic.log.info("Refocusing, pausing")
+        pulsedmasterlogic.pause_measurement()
+
+def on_refocused(tag,pos):
+    if pulsed_running() and pulsed_paused():
+        pulsedmasterlogic.log.info("Refocused, unpausing")
+        pulsedmasterlogic.continue_measurement()
+
+optimizerlogic.sigRefocusStarted.connect(on_refocusing)
+optimizerlogic.sigRefocusFinished.connect(on_refocused)
