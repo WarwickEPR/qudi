1915a6b23f898e722358f8ca8871e615e3e01722
Colin Stephen <C.J.Stephen@warwick.ac.uk>
2018-02-22 23:15:01 +0000

    Convenience scripts to change user

---
+++ tools/switch-to-bg.sh
@@ -0,0 +1,2 @@
+git config --replace-all --global user.name "Ben Green (Confocal)"
+git config --replace-all --global user.email "b.green@warwick.ac.uk"
---
+++ tools/switch-to-cjs.sh
@@ -0,0 +1,2 @@
+git config --replace-all --global user.name "Colin Stephen"
+git config --replace-all --global user.email "C.J.Stephen@warwick.ac.uk"
