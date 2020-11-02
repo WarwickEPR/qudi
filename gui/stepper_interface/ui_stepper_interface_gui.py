9fdefeca9c5db579bf6757b9bdeffceb11a60d1f
Ben Green <bengreen.uk@gmail.com>
2020-11-02 16:32:20 +0000

    basic interface for simply stepping attocube stages. a large number of assumptions built-in i.e. three axes, capacitance matters etc

---
+++ gui/stepper_interface/ui_stepper_interface_gui.py
@@ -0,0 +1,336 @@
+# -*- coding: utf-8 -*-
+"""
+This file contains the Qudi GUI module for ODMR control.
+
+Qudi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Qudi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Qudi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
+top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
+"""
+
+from qtpy import QtCore
+from qtpy import QtWidgets
+from qtpy import uic
+import numpy as np
+import os
+import time
+
+from core.connector import Connector
+from gui.guibase import GUIBase
+
+class StepperInterfaceMainWindow(QtWidgets.QMainWindow):
+    """ The main window for the stepper GUI.
+    """
+
+    sigPressKeyBoard = QtCore.Signal(QtCore.QEvent)
+
+    def __init__(self):
+        # Get the path to the *.ui file
+        this_dir = os.path.dirname(__file__)
+        ui_file = os.path.join(this_dir, 'ui_stepper_interface_gui.ui')
+
+        # Load it
+        super(StepperInterfaceMainWindow, self).__init__()
+        uic.loadUi(ui_file, self)
+        self.show()
+
+        def keyPressEvent(self, event):
+            """Pass the keyboard press event from the main window further. """
+            self.sigPressKeyBoard.emit(event)
+
+class StepperInterfaceGui(GUIBase):
+    """
+    This is the GUI Class for Stepper control
+    """
+
+    _modclass = 'StepperInterfaceGui'
+    _modtype = 'gui'
+
+    # declare connectors
+    stepperlogic1 = Connector(interface='StepperLogic')
+
+    def __init__(self, config, **kwargs):
+        super().__init__(config=config, **kwargs)
+
+    def on_activate(self):
+        """ Definition, configuration and initialisation of the Confocal Stepper GUI.
+
+        This init connects all the graphic modules, which were created in the
+        *.ui file and configures the event handling between the modules.
+        """
+
+        self._stepper_logic = self.stepperlogic1()
+        self.initMainUI()
+
+        ########################################################################
+        #                       Connect signals                                #
+        ########################################################################
+
+        # Show the Main SteppingConfocal GUI:
+        self.show()
+
+    def on_deactivate(self):
+        """ Reverse steps of activation
+
+        @return int: error code (0:OK, -1:error)
+        """
+        self._mw.close()
+        return 0
+
+    def initMainUI(self):
+        """ Definition, configuration and initialisation of the confocal stepper GUI.
+
+        This init connects all the graphic modules, which were created in the
+        *.ui file and configures the event handling between the modules.
+        Moreover it sets default values.
+        """
+        # Use the inherited class 'Ui_StepperGuiUI' to create now the GUI element:
+        self._mw = StepperInterfaceMainWindow()
+
+        ###################################################################
+        #               Configuring the dock widgets                      #
+        ###################################################################
+        # All our gui elements are dockable, and so there should be no "central" widget.
+        # self._mw.centralwidget.hide()
+        self._mw.setDockNestingEnabled(True)
+
+        # setup a timer to prevent someone asking the hardware to recalibrate during a move
+        self._timer = QtCore.QTimer()
+        self._timer.timeout.connect(self.reenable_hardware_buttons)
+
+        self.init_hardware_UI()
+        self._mw.move_up_button.clicked.connect(self.move_up_clicked)
+        self._mw.move_down_button.clicked.connect(self.move_down_clicked)
+        self._mw.move_left_button.clicked.connect(self.move_left_clicked)
+        self._mw.move_right_button.clicked.connect(self.move_right_clicked)
+        self._mw.raise_button.clicked.connect(self.raise_clicked)
+        self._mw.lower_button.clicked.connect(self.lower_clicked)
+        self._mw.stop_button.clicked.connect(self.stop_clicked)
+
+        self._mw.set_origin_button.clicked.connect(self.set_origin)
+
+    def init_hardware_UI(self):
+        # Set the range for the spin boxes of the voltage and frequency values:
+        constraints = self._stepper_logic.get_constraints()
+        (minvol, maxvol) = constraints['x']['voltage']
+        self._mw.x_amplitude_doubleSpinBox.setRange(minvol, maxvol)
+        (minvol, maxvol) = constraints['y']['voltage']
+        self._mw.y_amplitude_doubleSpinBox.setRange(minvol, maxvol)
+        (minvol, maxvol) = constraints['z']['voltage']
+        self._mw.z_amplitude_doubleSpinBox.setRange(minvol, maxvol)
+
+        (minfreq, maxfreq) = constraints['x']['frequency']
+        self._mw.x_frequency_spinBox.setRange(minfreq, maxfreq)
+        (minfreq, maxfreq) = constraints['y']['frequency']
+        self._mw.y_frequency_spinBox.setRange(minfreq, maxfreq)
+        (minfreq, maxfreq) = constraints['z']['frequency']
+        self._mw.z_frequency_spinBox.setRange(minfreq, maxfreq)
+
+        # set minimal steps for the current value
+        self._mw.x_amplitude_doubleSpinBox.setSingleStep(0.1)
+        self._mw.y_amplitude_doubleSpinBox.setSingleStep(0.1)
+        self._mw.z_amplitude_doubleSpinBox.setSingleStep(0.1)
+
+        # set unit in spin box
+        self._mw.x_amplitude_doubleSpinBox.setSuffix(" V")
+        self._mw.y_amplitude_doubleSpinBox.setSuffix(" V")
+        self._mw.z_amplitude_doubleSpinBox.setSuffix(" V")
+
+        self._mw.x_frequency_spinBox.setSuffix(" Hz")
+        self._mw.y_frequency_spinBox.setSuffix(" Hz")
+        self._mw.z_frequency_spinBox.setSuffix(" Hz")
+
+        # connect actions
+        self._mw.read_hardware_pushButton.clicked.connect(self.measure_stepper_hardware_values)
+        self._mw.update_hardware_pushButton.clicked.connect(self.update_stepper_hardware_values)
+
+        # get current step values
+        self.measure_stepper_hardware_values()
+
+    def show(self):
+        """Make window visible and put it above all other windows. """
+        self._mw.show()
+        self._mw.activateWindow()
+        self._mw.raise_()
+        return
+
+    def keyPressEvent(self, event):
+        """ Handles the passed keyboard events from the main window.
+
+        @param object event: qtpy.QtCore.QEvent object.
+        """
+        pass
+        # todo: update from key needs to be adjusted for confocal stepper
+        modifiers = QtWidgets.QApplication.keyboardModifiers()
+
+        position = self._scanning_logic.get_position()  # in meters
+        x_pos = position[0]
+        y_pos = position[1]
+        z_pos = position[2]
+
+        if modifiers == QtCore.Qt.ControlModifier:
+            if event.key() == QtCore.Qt.Key_Right:
+                self.update_from_key(x=float(round(x_pos + self.slider_big_step, 10)))
+            elif event.key() == QtCore.Qt.Key_Left:
+                self.update_from_key(x=float(round(x_pos - self.slider_big_step, 10)))
+            elif event.key() == QtCore.Qt.Key_Up:
+                self.update_from_key(y=float(round(y_pos + self.slider_big_step, 10)))
+            elif event.key() == QtCore.Qt.Key_Down:
+                self.update_from_key(y=float(round(y_pos - self.slider_big_step, 10)))
+            elif event.key() == QtCore.Qt.Key_PageUp:
+                self.update_from_key(z=float(round(z_pos + self.slider_big_step, 10)))
+            elif event.key() == QtCore.Qt.Key_PageDown:
+                self.update_from_key(z=float(round(z_pos - self.slider_big_step, 10)))
+            else:
+                event.ignore()
+        else:
+            if event.key() == QtCore.Qt.Key_Right:
+                self.update_from_key(x=float(round(x_pos + self.slider_small_step, 10)))
+            elif event.key() == QtCore.Qt.Key_Left:
+                self.update_from_key(x=float(round(x_pos - self.slider_small_step, 10)))
+            elif event.key() == QtCore.Qt.Key_Up:
+                self.update_from_key(y=float(round(y_pos + self.slider_small_step, 10)))
+            elif event.key() == QtCore.Qt.Key_Down:
+                self.update_from_key(y=float(round(y_pos - self.slider_small_step, 10)))
+            elif event.key() == QtCore.Qt.Key_PageUp:
+                self.update_from_key(z=float(round(z_pos + self.slider_small_step, 10)))
+            elif event.key() == QtCore.Qt.Key_PageDown:
+                self.update_from_key(z=float(round(z_pos - self.slider_small_step, 10)))
+            else:
+                event.ignore()
+
+    def update_stepper_hardware_values(self):
+        self.disable_hardware_buttons()
+        self._stepper_logic.set_voltage('x', self._mw.x_amplitude_doubleSpinBox.value())
+        self._stepper_logic.set_voltage('y', self._mw.y_amplitude_doubleSpinBox.value())
+        self._stepper_logic.set_voltage('z', self._mw.z_amplitude_doubleSpinBox.value())
+
+        self._stepper_logic.set_frequency('x', self._mw.x_frequency_spinBox.value())
+        self._stepper_logic.set_frequency('y', self._mw.y_frequency_spinBox.value())
+        self._stepper_logic.set_frequency('z', self._mw.z_frequency_spinBox.value())
+
+        if self._mw.z_dcin_checkBox.isChecked() is True:
+            self._stepper_logic.enable_DC_input('z')
+
+        axes_to_ground = []
+        if self._mw.x_grounded_checkBox.isChecked() is True:
+            axes_to_ground.append('x')
+        if self._mw.y_grounded_checkBox.isChecked() is True:
+            axes_to_ground.append('y')
+        if self._mw.z_grounded_checkBox.isChecked() is True:
+            axes_to_ground.append('z')
+        if len(axes_to_ground) != 0:
+            self._stepper_logic.ground(axes_to_ground)
+
+        time.sleep(0.5)
+        self.measure_stepper_hardware_values()
+
+    def measure_stepper_hardware_values(self):
+        parameters = self._stepper_logic.refresh_hardware_status()
+        xvals = parameters['x']
+        yvals = parameters['y']
+        zvals = parameters['z']
+
+        self._mw.x_amplitude_doubleSpinBox.setValue(xvals['voltage'])
+        self._mw.y_amplitude_doubleSpinBox.setValue(yvals['voltage'])
+        self._mw.z_amplitude_doubleSpinBox.setValue(zvals['voltage'])
+
+        self._mw.x_frequency_spinBox.setValue(xvals['frequency'])
+        self._mw.y_frequency_spinBox.setValue(yvals['frequency'])
+        self._mw.z_frequency_spinBox.setValue(zvals['frequency'])
+
+        self._mw.x_cap_label.setText(str(np.round(1e9 * xvals['capacitance'], 0)))
+        self._mw.y_cap_label.setText(str(np.round(1e9 * yvals['capacitance'], 0)))
+        self._mw.z_cap_label.setText(str(np.round(1e9 * zvals['capacitance'], 0)))
+
+        self._mw.x_grounded_checkBox.setChecked(xvals['grounded'])
+        self._mw.y_grounded_checkBox.setChecked(yvals['grounded'])
+        self._mw.z_grounded_checkBox.setChecked(zvals['grounded'])
+
+        self._mw.z_dcin_checkBox.setChecked(zvals['dci'])
+        self.update_positions()
+        self.reenable_hardware_buttons()
+
+    def update_positions(self):
+        pos = self._stepper_logic.get_current_location()
+        self._mw.x_pos_label.setText(str(pos['x']))
+        self._mw.y_pos_label.setText(str(pos['y']))
+        self._mw.z_pos_label.setText(str(pos['z']))
+
+    def stop_clicked(self):
+        self._stepper_logic.stop()
+        self.reenable_hardware_buttons()
+
+    def move_up_clicked(self):
+        self._stepper_logic.move_number_of_steps('y', self._mw.y_steps_spinbox.value())
+        self.disable_hardware_buttons()
+        self.update_positions()
+        self._timer.start(1000 * self._mw.y_steps_spinbox.value() / self._mw.y_frequency_spinBox.value())
+
+    def move_down_clicked(self):
+        self._stepper_logic.move_number_of_steps('y', -self._mw.y_steps_spinbox.value())
+        self.disable_hardware_buttons()
+        self.update_positions()
+        self._timer.start(1000 * self._mw.y_steps_spinbox.value() / self._mw.y_frequency_spinBox.value())
+
+    def move_left_clicked(self):
+        self._stepper_logic.move_number_of_steps('x', -self._mw.x_steps_spinbox.value())
+        self.disable_hardware_buttons()
+        self.update_positions()
+        self._timer.start(1000 * self._mw.x_steps_spinbox.value() / self._mw.x_frequency_spinBox.value())
+
+    def move_right_clicked(self):
+        self._stepper_logic.move_number_of_steps('x', self._mw.x_steps_spinbox.value())
+        self.disable_hardware_buttons()
+        self.update_positions()
+        self._timer.start(1000 * self._mw.x_steps_spinbox.value() / self._mw.x_frequency_spinBox.value())
+
+    def raise_clicked(self):
+        self._stepper_logic.move_number_of_steps('z', self._mw.z_steps_spinbox.value())
+        self.disable_hardware_buttons()
+        self.update_positions()
+        self._timer.start(1000 * self._mw.z_steps_spinbox.value() / self._mw.z_frequency_spinBox.value())
+
+    def lower_clicked(self):
+        self._stepper_logic.move_number_of_steps('z', -self._mw.z_steps_spinbox.value())
+        self.disable_hardware_buttons()
+        self.update_positions()
+        self._timer.start(1000 * self._mw.z_steps_spinbox.value() / self._mw.z_frequency_spinBox.value())
+
+    def reenable_hardware_buttons(self):
+        self._timer.stop()
+        self._mw.read_hardware_pushButton.setEnabled(True)
+        self._mw.update_hardware_pushButton.setEnabled(True)
+        self._mw.move_up_button.setEnabled(True)
+        self._mw.move_down_button.setEnabled(True)
+        self._mw.move_left_button.setEnabled(True)
+        self._mw.move_right_button.setEnabled(True)
+        self._mw.raise_button.setEnabled(True)
+        self._mw.lower_button.setEnabled(True)
+
+    def disable_hardware_buttons(self):
+        self._mw.read_hardware_pushButton.setEnabled(False)
+        self._mw.update_hardware_pushButton.setEnabled(False)
+        self._mw.read_hardware_pushButton.setEnabled(False)
+        self._mw.update_hardware_pushButton.setEnabled(False)
+        self._mw.move_up_button.setEnabled(False)
+        self._mw.move_down_button.setEnabled(False)
+        self._mw.move_left_button.setEnabled(False)
+        self._mw.move_right_button.setEnabled(False)
+        self._mw.raise_button.setEnabled(False)
+        self._mw.lower_button.setEnabled(False)
+
+    def set_origin(self):
+        self._stepper_logic.set_origin()
+        self.update_positions()
---
+++ logic/stepper_interface_logic.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+"""
+This file contains the Qudi stepper interface logic class.
+
+Qudi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Qudi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Qudi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
+top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
+"""
+
+from qtpy import QtCore
+from collections import OrderedDict
+import numpy as np
+import time
+
+from core.connector import Connector
+from logic.generic_logic import GenericLogic
+from core.util.mutex import Mutex
+
+
+class StepperLogic(GenericLogic):
+    """ This logic module allows simple UI control of hardware steppers (tailored for low temp attocube stages)
+
+    """
+    sigMoveNumberOfSteps = QtCore.Signal(str, int)
+    sigStop = QtCore.Signal()
+
+    # declare connectors
+    stepper1 = Connector(interface='MotorInterface')
+
+    def __init__(self, config, **kwargs):
+        """ Create StepperLogic object with connectors.
+
+        @param dict config: module configuration
+        @param dict kwargs: optional parameters
+        """
+        super().__init__(config=config, **kwargs)
+
+        #locking for thread safety
+        self.threadlock = Mutex()
+
+        # self.log.debug('The following configuration was found.')
+        #
+        # # checking for the right configuration
+        # for key in config.keys():
+        #     self.log.debug('{0}: {1}'.format(key, config[key]))
+
+        # # in bins
+        # self._count_length = 300
+        # self._smooth_window_length = 10
+        # self._counting_samples = 1      # oversampling
+        # # in hertz
+        # self._count_frequency = 50
+        return
+
+    def on_activate(self):
+        """ Initialisation performed during activation of the module.
+        """
+        # Connect to hardware and save logic
+        self._stepping_device = self.stepper1()
+        self.axes = self._stepping_device.axis()
+        #TODO might want to get the constraints to allow the UI to place limits
+        return
+
+    def on_deactivate(self):
+        """ Deinitialisation performed during deactivation of the module.
+        """
+         # self.sigCountDataNext.disconnect()
+        return
+
+    def get_constraints(self):
+        """ Returns the frequency and voltage limits for the connected axes """
+        constraints = self._stepping_device.get_constraints()
+        axes = self.axes
+        relevant_constraints = {}
+        for ax in axes:
+            relevant_constraints[ax] = {}
+            relevant_constraints[ax]['frequency'] = (constraints[ax]['vel_min'], constraints[ax]['vel_max'])
+            relevant_constraints[ax]['voltage'] = (constraints[ax]['acc_min'], constraints[ax]['acc_max'])
+
+        return relevant_constraints
+
+    def move_number_of_steps(self, axis, steps):
+        """ Move the specified number of steps in the specified axis ('x', 'y', or 'z')
+
+        @return the new current position for the specified axis"""
+        self._stepping_device.move_rel({axis: steps})
+        time.sleep(0.1)
+        return self._stepping_device.get_pos()[axis]
+
+    def get_current_location(self):
+        """ Return the current position for all axes in steps """
+        return self._stepping_device.get_pos()
+
+    def get_frequency(self, axis):
+        """ Get the frequency for the specified axis
+
+        @return the actual values set in hardware """
+        return self._stepping_device.frequency(axis)
+
+    def get_voltage(self, axis):
+        """ Get the voltage for the specified axis
+
+        @return the actual values set in hardware """
+        return self._stepping_device.voltage(axis)
+
+    def set_frequency(self, axis, frequency):
+        """ Set the frequency for the given axis
+
+        @return the actual values set in hardware """
+        self._stepping_device.frequency(axis, value=frequency)
+        time.sleep(0.1)
+        return self.get_frequency(axis)
+
+    def set_voltage(self, axis, voltage):
+        """ Set the voltage for the given axis
+
+        @return the actual values set in hardware """
+        self._stepping_device.voltage(axis, value=voltage)
+        time.sleep(0.1)
+        return self.get_voltage(axis)
+
+    def get_dci_status(self):
+        """ Get the DC Input status for all axes """
+        return self._stepping_device.get_DC_input_status(self.axes)
+
+    def enable_DC_input(self, axis):
+        """ Enable DC inputs for the specified axes. Note that these are automatically
+        disabled once you step the axis"""
+        self._stepping_device.enable_DC_input(axis)
+
+    def disable_DC_input(self, axis):
+        """ Disable DC inputs for the specified axes. Note that these are automatically
+        disabled once you step the axis"""
+        self._stepping_device.disable_DC_input(axis)
+
+    def ground(self, axis):
+        """ Ground the specified axes """
+        self._stepping_device.ground(axis)
+
+    def unground(self, axis):
+        """ Unground the specified axes """
+        self._stepping_device.unground(axis)
+
+    def get_grounding_status(self):
+        """ Get the grounding status for all axes """
+        return self._stepping_device.get_grounding_status(self.axes)
+
+    def measure_capacitance(self):
+        """ Measure the capacitance for all connected axes
+
+        @return capacitance in F """
+        return self._stepping_device.capacitance(self.axes)
+
+    def refresh_hardware_status(self):
+        """ Update hardware configuration values for all axes """
+        freq = self.get_frequency(self.axes)
+        voltage = self.get_voltage(self.axes)
+        capacitance = self.measure_capacitance()
+        dci = self.get_dci_status()
+        grounded = self.get_grounding_status()
+
+        hardware_values = {}
+        for idx, ax in enumerate(self.axes):
+            hardware_values[ax] = {}
+            hardware_values[ax]['capacitance'] = capacitance[idx]
+            hardware_values[ax]['frequency'] = freq[idx]
+            hardware_values[ax]['voltage'] = voltage[idx]
+            hardware_values[ax]['dci'] = dci[idx]
+            hardware_values[ax]['grounded'] = grounded[idx]
+
+        return hardware_values
+
+    def set_origin(self):
+        """ Set the origin of the stage system """
+        self._stepping_device.calibrate()
+
+    def stop(self):
+        """ Stop all movement """
+        self._stepping_device.abort()
