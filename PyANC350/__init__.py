09ce66a8c7141c4da172bc86e53e60de026706e4
Bor406 <82883941+Bor406@users.noreply.github.com>
2021-04-27 18:27:32 +0100

    Added ctypes

--- PyANC350/__init__.py
+++ PyANC350/__init__.py
@@ -1,1 +1,5 @@
+# Not sure why this is needed, but module will not work without it.
+import ctypes
+ctypes.windll.LoadLibrary("c:\\Users\\User\\Documents\\Qudi-IX\\PyANC350\\anc350v4.dll")
+
 from PyANC350.PyANC350v4 import *
