Fixes to arb scanning, including ensuring the rotation axis is 'up'

logic/scan_patterns/hdf5_scan.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import numpy as np
 from logic.zmq.data.tables_context import TablesContext
+from logic.zmq.data.timestamp import Timestamp
 import logic.scan_patterns.general_scan as scan
 from queue import Queue
 from tables.atom import Float64Atom
@@ -8,8 +9,8 @@ from tables.atom import Float64Atom
 
 # not the most efficient way but the flat iterator doesn't seem to be fully implemented for tables.Array as ndarray
 def write_to_h5array_3d(target, indices, data):
-    for i, j, d, c in zip(*np.unravel_index(indices, shape=target.shape), data):
-        target[i, j, 1, d] = c
+    for i, j, _, d, c in zip(*np.unravel_index(indices, shape=target.shape), data):
+        target[i, j, 0, d] = c
 
 
 def write_to_h5array_4d(target, indices, data):
@@ -35,7 +36,7 @@ class ParallelepipedScan(scan.ParallelepipedScan):
 
     def initialise_data(self):
         with self._tc as th:
-            ts = self._tc.timestamp()
+            ts = Timestamp.get_timestamp()
             name = 'ABC_{}'.format(ts)
             arr = th.tables.create_array("/Confocal/ABC",
                                          name,
@@ -102,7 +103,7 @@ class ParallelogramScan(scan.ParallelogramScan):
 
     def initialise_data(self):
         with self._tc as th:
-            ts = self._tc.timestamp()
+            ts = Timestamp.get_timestamp()
             name = 'AB_{}'.format(ts)
             arr = th.tables.create_array("/Confocal/AB",
                                          name,

logic/zmq/client/scan.py
@@ -12,7 +12,10 @@ class Tilt:
 
         # rotation axis and angle between stage z and tilted z
         z0 = np.array([0, 0, 1])
-        p, q, r = axis = self._rotation_axis = np.cross(z0, self._norm)
+        axis = np.cross(z0, self._norm)
+        if axis[0] < 0 or axis[1] < 0:
+            axis = -axis
+        p, q, r = self._rotation_axis = axis
         c = np.dot(z0, self._norm)
         s = np.sqrt(1-c*c)
         self._rotation_angle = np.arccos(c)